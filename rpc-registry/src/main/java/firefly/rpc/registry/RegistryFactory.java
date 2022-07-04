f2d5e0a02324bf32b6415c1fcd0333d3ecb866d4
rongguilin <rongjialin@iyunmai.com>
2022-07-04 13:47:25 +0800

    创建注册中心

--- rpc-registry/src/main/java/firefly/rpc/registry/RegistryFactory.java
+++ rpc-registry/src/main/java/firefly/rpc/registry/RegistryFactory.java
@@ -3,12 +3,12 @@ package firefly.rpc.registry;
 public class RegistryFactory {
     private static volatile RegistryService registryService;
     
-    public static RegistryService getInstance(String registryAddr, RegistryType type) {
+    public static RegistryService getInstance(String registryAddr, RegistryType type) throws Exception {
         if (null == registryService) {
             synchronized (RegistryService.class) {
                 if (null == registryService) {
                     if (type == RegistryType.ZOOKEEPER) {
-                        registryService = null;
+                        registryService = new ZookeeperRegistryService(registryAddr);
                     }
                     if (type == RegistryType.EUREKA) {
                         registryService = null;
